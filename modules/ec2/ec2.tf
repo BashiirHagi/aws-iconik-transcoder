cf7e3f2672be574f52bcbd5e68e7859e79132d7a
BashiirHagi <bashiir854@gmail.com>
2025-07-31 10:15:06 +0100

    Added tags to ASG

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -23,6 +23,12 @@ resource "aws_autoscaling_group" "transcoder-asg" {
     version = "$Latest"
   }
 
+  tag {
+    key   = "Name"
+    value = "iconik-transcoder-instances"
+
+    propagate_at_launch = true
+  }
 }
 
 resource "aws_security_group" "iconik_sg" {
